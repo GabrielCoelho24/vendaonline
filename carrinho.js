975d1b47de7ff8ff068f01afab68bc18032883e5
GabrielCoelho24 <142925248+GabrielCoelho24@users.noreply.github.com>
2023-11-27 19:45:42 +0000

    3

--- carrinho.js
+++ carrinho.js
@@ -49,7 +49,7 @@ if (document.readyState == 'loading') {
     const productImage = productInfos.getElementsByClassName("product-image")[0].src
     const productName = productInfos.getElementsByClassName("product-title")[0].innerText
     const productPrice = productInfos.getElementsByClassName("product-price")[0].innerText
-    
+
     const productsCartNames = document.getElementsByClassName("cart-product-title")
     for (var i = 0; i < productsCartNames.length; i++) {
       if (productsCartNames[i].innerText === productName) {
@@ -118,3 +118,92 @@ if (document.readyState == 'loading') {
     totalAmount = totalAmount.replace(".", ",")
     document.querySelector(".cart-total-container span").innerText = "R$" + totalAmount
   }
+
+  function validarFormulario(event) {
+    event.preventDefault(); // Impedir a ação padrão do formulário
+
+    var nome = document.forms["inscricao"]["nome"].value;
+    var cpf = document.forms["inscricao"]["cpf"].value;
+    var email = document.forms["inscricao"]["email"].value;
+    var telefone = document.forms["inscricao"]["telefone"].value;
+    var cep = document.forms["inscricao"]["cep"].value;
+
+    // Validar se os campos obrigatórios estão preenchidos
+    if (nome === "" || cpf === "" || email === "" || telefone === ""|| cep === "") {
+        alert("Todos os campos obrigatórios devem ser preenchidos.");
+        return false;
+    }
+
+    // Validar o formato do CPF
+    var cpfPattern = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
+    if (!cpf.match(cpfPattern)) {
+        alert("O CPF deve estar no formato XXX.XXX.XXX-XX.");
+        return false;
+    }
+
+    // Validar o formato do email
+    var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!email.match(emailPattern)) {
+        alert("O email deve estar em um formato válido.");
+        
+    }
+
+    var telefonePattern = /[0-9]{2} [0-9]{4}-[0-9]{4} /;
+    if (!telefone.match(telefonePattern)){
+        alert("o Telefone deve estar no formato XX XXXX-XXXX")
+        return false;
+    }
+
+    var cepPattern = /[0-9]{5}-[0-9]{3}/;
+    if (!telefone.match(cepPattern)){
+        alert("o Cep deve estar no formato XXXXX-XXX")
+        return false;
+    }
+
+
+    // Se todos os campos estiverem corretos, salvar as informações localmente
+    salvarInformacoesLocalmente();
+}
+
+function salvarInformacoesLocalmente() {
+    var nome = document.forms["inscricao"]["nome"].value;
+    var cpf = document.forms["inscricao"]["cpf"].value;
+    var email = document.forms["inscricao"]["email"].value;
+    var telefone = document.forms["inscricao"]["telefone"].value;
+    var cep = document.forms["inscricao"]["cep"].value;
+
+    // Salvar as informações localmente (por exemplo, no LocalStorage)
+    var inscricao = {
+        nome: nome,
+        cpf: cpf,
+        email: email,
+        telefone: telefone,
+        cep: cep
+    };
+
+    // Converter o objeto para uma string JSON
+    var inscricaoJSON = JSON.stringify(inscricao);
+
+    // Armazenar os dados localmente
+    localStorage.setItem("inscricao", inscricaoJSON);
+
+    // Exibir uma mensagem de inscrição bem-sucedida
+    alert("Inscrição efetuada com sucesso");
+}
+
+// Verificar se já existe a chave "total_inscritos" no Local Storage
+if (localStorage.getItem("total_inscritos") === null) {
+    // Se não existir, criar a chave "total_inscritos" com valor inicial 0
+    localStorage.setItem("total_inscritos", 0);
+}
+
+// Ao efetuar uma nova inscrição, aumentar o valor da chave "total_inscritos" em 1
+var totalInscritos = parseInt(localStorage.getItem("total_inscritos"));
+totalInscritos = isNaN(totalInscritos) ? 0 : totalInscritos;
+totalInscritos++;
+localStorage.setItem("total_inscritos", totalInscritos);
+
+// Para recuperar o número total de inscritos:
+var numeroTotalInscritos = localStorage.getItem("total_inscritos");
+
+console.log("Número total de inscritos: " + numeroTotalInscritos);
